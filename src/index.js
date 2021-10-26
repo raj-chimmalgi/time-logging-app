4afd815b8238839ec9c552962dee8f7c6bf4f0b3
Raj Chimmalgi <chimmalgi@gmail.com>
2021-10-25 21:38:46 -0700

    created all components using static data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TimersDashboard from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TimersDashboard />
   </React.StrictMode>,
   document.getElementById('content')
 );
